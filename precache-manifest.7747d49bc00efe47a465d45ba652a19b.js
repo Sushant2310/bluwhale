88e0f961da4aa811cc879baffe3667a4b57e96ea
Josue Matos <josuematos87@gmail.com>
2020-08-18 18:10:13 -0500

    Updates

---
+++ precache-manifest.7747d49bc00efe47a465d45ba652a19b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4f634c7d8007a4b844b1bd740fa64e",
+    "url": "/bluwhale/index.html"
+  },
+  {
+    "revision": "b442ebc24254d01e7bc6",
+    "url": "/bluwhale/static/css/main.ac49d2bf.chunk.css"
+  },
+  {
+    "revision": "d4cfb06d85dd71ee18c5",
+    "url": "/bluwhale/static/js/2.b3251344.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bluwhale/static/js/2.b3251344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b442ebc24254d01e7bc6",
+    "url": "/bluwhale/static/js/main.0f2d9805.chunk.js"
+  },
+  {
+    "revision": "e588f030bbf20d0e9247",
+    "url": "/bluwhale/static/js/runtime-main.8c7a940e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bluwhale/static/media/logo.5d5d9eef.svg"
+  }
+]);
